4b1039f17c0a0b707cfc7aa1a3a2b5ba4d441753
strax <bogdan2003bk96@gmail.com>
2022-01-09 14:37:06 +0200

    test

---
+++ main.tf
@@ -0,0 +1,93 @@
+provider "aws" {
+  region = "us-east-2"
+  # access_key = "AKIASZWIO7VWKWGGFO37"
+  # secret_key = "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"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_eip" "lb" {
+  instance = aws_instance.docker_site.id
+  vpc      = true
+}
+
+resource "aws_instance" "docker_site" {
+  ami                         = data.aws_ami.ubuntu.id
+  instance_type               = "t2.micro"
+  vpc_security_group_ids      = [aws_security_group.allow_web.id]
+  key_name                    = "docker_site"
+
+    connection {
+      type        = "ssh"
+      host        = self.public_ip
+      user        = "ubuntu"
+      private_key = file("docker_site.pem")
+}
+
+    provisioner "remote-exec" {
+    inline = [
+    "sudo apt-get update && install curl",
+    "curl -sSL https://get.docker.com/ | sh",
+    "sudo docker run -d --name docker_site -p 80:80 DiscofromKPI/DevOps_prac",
+    "sudo docker run -d --name watchtower -v /var/run/docker.sock:/var/run/docker.sock containrrr/watchtower --cleanup -i 10"
+    ]
+  }
+
+  tags = {
+    Name = "docker_site"
+  }
+}
+
+resource "aws_security_group" "allow_web" {
+  name        = "allow_web_traffic"
+  description = "Allow Web inbound traffic"
+
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+output "Elastic_IP" {
+  value = aws_instance.docker_site.public_ip
+}
